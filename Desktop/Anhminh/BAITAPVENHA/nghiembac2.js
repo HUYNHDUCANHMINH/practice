c89372a481a8d4935e80a24e25727e867c2cb68b
user <user@usernoMacBook-Pro.local>
2022-05-04 11:13:27 +0900

    add3

--- Desktop/Anhminh/BAITAPVENHA/nghiembac2.js
+++ Desktop/Anhminh/BAITAPVENHA/nghiembac2.js
@@ -7,23 +7,18 @@ function Test3 () {
     //c = parseInt(c);
     //let c_a = c/a
     //let x_1 = (-c)/b
-    let delta = Math.pow(b,2) - 4*a*c
+    let delta = b*b - 4*a*c
+    let sqrtdelta = Math.sqrt(delta)
         if (a == 0) {
-            if (b == 0) {
-                if ( c == 0) {
-                    document.getElementById("nghiem").innerHTML= " 1 Phuong trinh vo so nghiem";
-                } else {
-                    document.getElementById("nghiem").innerHTML= " 1 Phuong trinh vo nghia";
-                } else {
-                    
-                }
-            if (b == 0 && c >=0) {
-                    document.getElementById("nghiem").innerHTML= " 2 Phuong trinh vo nghiem" 
-            }else {
-                document.getElementById("nghiem").innerHTML= " 2 Phuong trinh nghiem kep" + Math.sqrt(-c/a)
-            }if (delta >=0) {
-                document.getElementById("nghiem").innerHTML= "x1 = " + ((-b + Math.sqrt(delta))/2 * a).toFixed(2) + " x2 = " + ((-b - Math.sqrt(delta))/2 * a).toFixed(2);
-            }else {
-                document.getElementById("nghiem").innerHTML= " 3 Phuong trinh vo nghiem" 
-            }
+            b != 0 && c != 0 ? document.getElementById("nghiem").innerHTML= -c/b
+            : document.getElementById("nghiem").innerHTML= " Phuong trinh vo so nghiem"
+        }
+            else
+                if (b != 0) {
+            delta = 0 ? document.getElementById("nghiem").innerHTML= -b/(2*a) 
+            : delta > 0 ? document.getElementById("nghiem").innerHTML= "x1 = " + ((-b + sqrtdelta)/(2 * a)).toFixed(2) + " x2 = " + ((-b - sqrtdelta)/(2 * a)).toFixed(2) 
+            : document.getElementById("nghiem").innerHTML= " 1 Phuong trinh vo nghiem "
+            }   else 
+                    c/a >= 0 ? document.getElementById("nghiem").innerHTML= " 2 Phuong trinh vo nghiem "
+            : document.getElementById("nghiem").innerHTML= " 2 Phuong trinh nghiem kep " + Math.sqrt(-c/a);
         }
