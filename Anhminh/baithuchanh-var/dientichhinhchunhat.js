b0e350aa89b6f085996c82f04c14e985f8da3e8a
HUYNHDUCANHMINH <102814481+HUYNHDUCANHMINH@users.noreply.github.com>
2022-05-05 09:49:43 +0900

    Delete Anhminh directory
    Renew

--- Anhminh/baithuchanh-var/dientichhinhchunhat.js
+++
@@ -1,3 +0,0 @@
-let width = +prompt("Nhap do rong cua hinh chu nhat la: ")
-let height = +prompt("Nhap do cao cua hinh chu nhat la: ")
-document.write("Dien tich hinh chu nhat la " + width * height);
